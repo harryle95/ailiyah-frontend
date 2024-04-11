10854d9264c954a95f22ad723a9b080c1b0fd2d5
harryle <lehoangsonsg@gmail.com>
2024-04-11 22:38:35 +0930

    feat: update PromptPanel to use PromptForm

--- src/components/PromptPanel.jsx
+++ src/components/PromptPanel.jsx
@@ -1,10 +1,9 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
-import { Form } from "react-router-dom";
+
+import PromptForm from "./PromptForm";
 
 
 export function PromptPanel({ history }) {
-    const [image, setImage] = useState();
     return (
         <div
             id="prompt-panel"
@@ -35,57 +34,11 @@ export function PromptPanel({ history }) {
                         </div>
                     ))
                 }
+
+                
             </div>
-
-            {/* Prompt and input image  */}
-            <Form
-                method="POST"
-                action="generate"
-                className="flex flex-col gap-y-4 px-4 overflow-auto max-h-[300px]"
-                id="prompt_input"
-            >
-                {image ?
-                    <div
-                        className="max-h-36 max-w-36"
-                    >
-                        <img src={image} />
-                    </div> : <></>}
-                <textarea name="prompt" className="text-black px-4 w-full bg-transparent py-2 border border-black rounded"></textarea>
-                <input
-                    className="hidden"
-                    type="file"
-                    id="file-upload"
-                    accept="image/*"
-                    name="input_image"
-                    onChange={(e) => { setImage(URL.createObjectURL(e.target.files[0])) }}
-                />
-
-                <div className="flex items-center px-4">
-                    {/* Upload Button */}
-                    <label
-                        className="w-1/3 h-12 mx-auto min-h-12 flex justify-center items-center rounded-lg border-solid border-2 bg-slate-500 hover:bg-slate-600"
-                        htmlFor="file-upload">
-                        Upload Image
-                    </label>
-
-                    {/* Remove Image Button */}
-                    <div className="w-1/3 h-12 mx-auto min-h-12 flex justify-center items-center rounded-lg border-solid border-2 bg-slate-500 hover:bg-slate-600">
-                        <button
-                            type="button"
-                            // Remove Preview image and set files to null
-                            onClick={() => {
-                                setImage(null);
-                                document.getElementById("file-upload").value = ''
-                            }}
-                        >Remove Image</button>
-                    </div>
-
-                    {/* Generate Button */}
-                    <div className="w-1/3 h-12 mx-auto min-h-12 flex justify-center items-center rounded-lg border-solid border-2 bg-slate-500 hover:bg-slate-600">
-                        <button type="submit">Generate</button>
-                    </div>
-                </div>
-            </Form>
+            {/* Prompt Form */}
+            <PromptForm initPrompt={""}/>
         </div>
     )
 }
