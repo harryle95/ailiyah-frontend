bed038a31564b16473b51c0284ccdcd759653ea2
hoangson <lehoangsonsg@gmail.com>
2024-04-13 14:46:20 +0930

    feat: set buttons to appear when active when hovered and when link is active

--- src/components/SideBarListItem.jsx
+++ src/components/SideBarListItem.jsx
@@ -1,20 +1,27 @@
 /* eslint-disable react/prop-types */
-import { NavLink, Form } from "react-router-dom";
+import { NavLink, Form, useLocation } from "react-router-dom";
 import deleteIcon from "../resources/delete.png";
 import editIcon from "../resources/edit.png";
 import { useState } from "react";
+
 
 export function SideBarListItem({ listItem }) {
   const itemId = listItem.id
   const itemName = listItem.name
   const [isEditing, setEditing] = useState(false);
   const [projectName, setProjectName] = useState(listItem.name);
+
+  let location = useLocation();
+
+  let isActive = location.pathname === `/project/${itemId}`
+  let buttonClass = isActive?"w-1/6 items-center flex":"w-1/6 items-center hidden group-hover:flex"
+
   return (
-    <div className="flex items-center gap-x-2">
+    <div className="group flex items-center gap-x-2">
       {/* Edit project name form or project name */}
       {
         isEditing ?
-          <Form method="PUT">
+          <Form method="PUT" onSubmit={() => setEditing(!isEditing)}>
             <input name="id" value={itemId} className="hidden" readOnly />
             <input
               name="name"
@@ -23,18 +30,16 @@ export function SideBarListItem({ listItem }) {
               placeholder={projectName}
               onChange={e => setProjectName(e.target.value)}
             />
-            <button type="submit" className='hidden' id={`edit-${itemId}`} />
+            <input type="submit" id={`edit-${itemId}`} className="hidden"></input>
           </Form> :
           <NavLink
+            id={`project/${itemId}`}
             to={`/project/${itemId}`}
-            className={({ isActive }) => {
-              let base = "w-full overflow-clip px-2 py-1 border-[#c432f5] border-solid "
-              if (isActive) {
-                return base + "border-2 rounded-md"
-              } else {
-                return base
-              }
-            }}
+            className={({ isActive }) => isActive ?
+              "w-full overflow-clip px-2 py-1 rounded-md border-[#c432f5] border-solid border-2 active"
+              :
+              "w-full overflow-clip px-2 py-1 rounded-md"
+            }
           >
             <p className="w-full whitespace-nowrap overflow-clip">{itemName}</p>
           </NavLink>
@@ -42,23 +47,25 @@ export function SideBarListItem({ listItem }) {
 
       {/* Edit and Delete Buttons */}
       {/* Edit Button - switch editing modes and submit form if in editting mode */}
-      <div className="w-1/6 flex items-center">
-        <button
-          type="submit"
-          form={`edit-${itemId}`}
-          onClick={(e) => {
-            if (isEditing) {
-              
-              document.getElementById(`edit-${itemId}`).click()
-            }else{
-              e.preventDefault()
-            }
-            setEditing(!isEditing)
-          }
-          }
-        >
-          <img className="min-w-5 max-w-5" src={editIcon} alt="editIcon" />
-        </button>
+      <div
+        className={buttonClass}
+      >
+        {
+          isEditing ?
+            <button
+              name="submit"
+              onClick={() => document.getElementById(`edit-${itemId}`).click()}
+            >
+              <img className="min-w-5 max-w-5" src={editIcon} alt="editIcon" />
+            </button> :
+            <button
+              name="edit"
+              onClick={() => { setEditing(!isEditing); console.log(isEditing) }}
+            >
+              <img className="min-w-5 max-w-5" src={editIcon} alt="editIcon" />
+            </button>
+        }
+
 
         {/* Delete Button */}
         <Form method="DELETE" id={`delete-${itemId}`}>
@@ -67,7 +74,7 @@ export function SideBarListItem({ listItem }) {
 
         {/* Delete button - put outside form for alignment */}
         <button form={`delete-${itemId}`} type="submit">
-          <img src={deleteIcon} className="min-w-5 max-w-5" alt="deleteIcon"/>
+          <img src={deleteIcon} className="min-w-5 max-w-5" alt="deleteIcon" />
         </button>
       </div>
 
