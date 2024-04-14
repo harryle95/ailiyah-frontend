cba1e47d5cb28e7fca24b2766128fd44d833ba49
hoangson <lehoangsonsg@gmail.com>
2024-04-14 09:38:20 +0930

    feat: fix request create form not working correctly

--- src/components/PromptForm.jsx
+++ src/components/PromptForm.jsx
@@ -4,12 +4,14 @@ import { useState } from "react";
 // eslint-disable-next-line react/prop-types
 export default function PromptForm({ initImage, initPrompt, projectId }) {
     const [thumbnail, setThumbnail] = useState(initImage);
-    const [formData, setFormData] = useState({project_id: projectId, prompt: initPrompt, file: null});
+    const [formData, setFormData] = useState({ project_id: projectId, prompt: initPrompt, file: null });
     const submit = useSubmit()
     return (
         <Form
             className="flex flex-col gap-y-4 px-4 overflow-auto max-h-[300px]"
+            method="POST"
         >
+            <input type="text" className="hidden" name="project_id" value={projectId} readOnly />
             {/* Thumbnail */}
             {thumbnail ?
                 <div
@@ -64,20 +66,14 @@ export default function PromptForm({ initImage, initPrompt, projectId }) {
                 {/* TODO: fix form submission to do navigation use onSubmit instead of an onclick button */}
                 <div className="w-1/3 h-12 mx-auto min-h-12 flex justify-center items-center rounded-lg border-solid border-2 bg-slate-500 hover:bg-slate-600">
                     <button
-                        type="submit"
-                        onClick={() => {
+                        onClick={(e) => {
+                            // Need to prevent default here - i.e. submit the form snce button is inside the form 
+                            e.preventDefault(); 
                             let submitFormData = new FormData();
                             for (const key in formData) {
-                                console.log(key, formData[key])
                                 submitFormData.append(key, formData[key])
                             }
-
-                            submit(submitFormData, {
-                                encType: "multipart/form-data",
-                                method:"POST",
-                                action:"generate"
-                            })
-
+                            submit(submitFormData, { encType: "multipart/form-data", method: "POST", action: `/project/${projectId}/create` })
                         }
                         }
                     >
