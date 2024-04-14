acbe8b173d6bf3eda86175e9fdfdccd05d32bdf1
hoangson <lehoangsonsg@gmail.com>
2024-04-14 09:39:48 +0930

    feat: add request loader and actions

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,7 +1,13 @@
 
 
 
-const REQUEST_URL = "http://127.0.0.1:8000/request";
+export const REQUEST_URL = "http://127.0.0.1:8000/request";
+
+export interface RequestDTO{
+    id: string
+    name: string
+    output_image: string
+}
 
 export async function createRequest(data: FormData){
     const coroutine = await fetch(REQUEST_URL, {
@@ -15,4 +21,10 @@ export async function createRequest(data: FormData){
 export async function deleteRequest(id: string){
     const coroutine = await fetch(`${REQUEST_URL}/${id}`, {method: "DELETE"})
     console.log(coroutine.status);
+}
+
+export async function getRequestByID(id: string): Promise<RequestDTO> {
+    const coroutine = await fetch(`${REQUEST_URL}/${id}`)
+    const result = await coroutine.json();
+    return result
 }
--- src/services/request_helpers.ts
+++ src/services/request_helpers.ts
@@ -1,11 +1,26 @@
-import { createRequest, deleteRequest } from "./request"
+import { redirect} from "react-router-dom"
+import { createRequest, deleteRequest, getRequestByID } from "./request"
 
 export async function actionMakeRequest({request, params}){
     const data = await request.formData()
-    return await createRequest(data)
+    console.log(Object.fromEntries(data))
+    const result = await createRequest(data)
+    console.log({request})
+    return null
+}
+
+export async function actionLogRequest({request, params}){
+    const data = await request.formData()
+    console.log(Object.fromEntries(data))
+    return null
 }
 
 export async function actionHandleRequest({request, params}){
     await deleteRequest(params.requestId)
-    return null
+    return redirect(`/project/${params.projectId}`)
+}
+
+export async function loaderRequestID({request, params}){
+    console.log("Get output image of request")
+    return await getRequestByID(params.requestId)
 }
