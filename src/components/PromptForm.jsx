559b99d2e379ef501ca3f8824d9b0f4831756de2
hoangson <lehoangsonsg@gmail.com>
2024-04-14 15:45:44 +0930

    feat: move generate and upload buttons inside text area MCIP-50

--- src/components/PromptForm.jsx
+++ src/components/PromptForm.jsx
@@ -1,15 +1,38 @@
 import { Form, useSubmit } from "react-router-dom";
 import { useState } from "react";
+import uploadIcon from "../resources/upload.png"
+import submitIcon from "../resources/submit.png"
+
 
 // eslint-disable-next-line react/prop-types
 export default function PromptForm({ initImage, initPrompt, projectId }) {
     const [thumbnail, setThumbnail] = useState(initImage);
     const [formData, setFormData] = useState({ project_id: projectId, prompt: initPrompt, file: null });
     const submit = useSubmit()
+
+    // Handler 
+    const onSubmitHandler = (e) => {
+        // Need to prevent default here - i.e. submit the form snce button is inside the form 
+        e.preventDefault();
+        let submitFormData = new FormData();
+        for (const key in formData) {
+            submitFormData.append(key, formData[key])
+        }
+        submit(submitFormData, { encType: "multipart/form-data", method: "POST", action: `/project/${projectId}/create` })
+    }
+
+    const onRemoveHandler = () => {
+        setThumbnail(null);
+        setFormData({ ...formData, file: null })
+        document.getElementById("file-upload").value = null // clear file-upload files for onChange behavior
+    }
+
+
     return (
         <Form
-            className="flex flex-col gap-y-4 px-4 overflow-auto max-h-[300px]"
+            className="flex flex-col gap-y-4 px-4 max-h-[300px]"
             method="POST"
+            onSubmit={onSubmitHandler}
         >
             <input type="text" className="hidden" name="project_id" value={projectId} readOnly />
             {/* Thumbnail */}
@@ -22,12 +45,35 @@ export default function PromptForm({ initImage, initPrompt, projectId }) {
             }
 
             {/* Prompt */}
-            <textarea
-                name="prompt"
-                className="text-black px-4 w-full bg-transparent py-2 border border-black rounded"
-                value={formData.prompt}
-                onChange={e => setFormData({ ...formData, prompt: e.target.value })}
-            />
+            <div className="relative">
+                <textarea
+                    name="prompt"
+                    className="text-black px-4 w-full bg-transparent py-2 border border-black rounded-lg max-h-52 resize-none"
+                    value={formData.prompt}
+                    onChange={e => setFormData({ ...formData, prompt: e.target.value })}
+                />
+                <div className="flex absolute bottom-2 right-2">
+                    <div className="w-7">
+                        <label htmlFor="file-upload">
+                            <img src={uploadIcon} alt="" />
+                        </label>
+
+                    </div>
+                    {formData.prompt === "" ?
+                        <div className="w-7">
+                            <button type="submit" disabled>
+                                <img src={submitIcon} alt="" />
+                            </button>
+                        </div> :
+                        <div className="w-7">
+                            <button type="submit">
+                                <img src={submitIcon} alt="" />
+                            </button>
+                        </div>
+                    }
+                </div>
+            </div>
+
             <input
                 className="hidden"
                 type="file"
@@ -40,47 +86,6 @@ export default function PromptForm({ initImage, initPrompt, projectId }) {
                 }
                 }
             />
-
-            <div className="flex items-center px-4">
-                {/* Upload Button */}
-                <label
-                    className="w-1/3 h-12 mx-auto min-h-12 flex justify-center items-center rounded-lg border-solid border-2 bg-slate-500 hover:bg-slate-600"
-                    htmlFor="file-upload">
-                    Upload Image
-                </label>
-
-                {/* Remove Image Button */}
-                <div className="w-1/3 h-12 mx-auto min-h-12 flex justify-center items-center rounded-lg border-solid border-2 bg-slate-500 hover:bg-slate-600">
-                    <button
-                        type="button"
-                        // Remove Preview image and set files to null
-                        onClick={() => {
-                            setThumbnail(null);
-                            setFormData({ ...formData, file: null })
-                            document.getElementById("file-upload").value = null // clear file-upload files for onChange behavior
-                        }}
-                    >Remove Image</button>
-                </div>
-
-                {/* Generate Button */}
-                {/* TODO: fix form submission to do navigation use onSubmit instead of an onclick button */}
-                <div className="w-1/3 h-12 mx-auto min-h-12 flex justify-center items-center rounded-lg border-solid border-2 bg-slate-500 hover:bg-slate-600">
-                    <button
-                        onClick={(e) => {
-                            // Need to prevent default here - i.e. submit the form snce button is inside the form 
-                            e.preventDefault(); 
-                            let submitFormData = new FormData();
-                            for (const key in formData) {
-                                submitFormData.append(key, formData[key])
-                            }
-                            submit(submitFormData, { encType: "multipart/form-data", method: "POST", action: `/project/${projectId}/create` })
-                        }
-                        }
-                    >
-                        Generate
-                    </button>
-                </div>
-            </div>
-        </Form>
+        </Form >
     )
 }
