88000b3136021265860a916a72c2ec017635c2a5
hoangson <lehoangsonsg@gmail.com>
2024-04-15 01:03:10 +0930

    feat: add sidebar show/hide feature

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { SideBar } from "../components/SideBar";
 import { Outlet } from "react-router-dom";
-
+import SButton from "../components/SComponents/SButton";
+import editIcon from "../resources/edit.png";
+import editHoveredIcon from "../resources/edit_hover.png";
 
 export default function Root() {
     const [isVisible, setVisible] = useState(true)
@@ -9,9 +11,13 @@ export default function Root() {
         <div className="flex">
             <SideBar isVisible={isVisible} />
             <div className="flex-grow-1 w-full relative">
-                <div className="absolute left-0 top-1/2" onClick={()=>setVisible(!isVisible)}>
-                    Shrink
-                </div>
+                <SButton
+                    className="absolute left-0 top-1/2 z-40"
+                    imageClass="w-10"
+                    onClick={()=>setVisible(!isVisible)}
+                    normalIcon={editIcon}
+                    hoverIcon={editHoveredIcon}
+                />
                 <Outlet />
             </div>
 
