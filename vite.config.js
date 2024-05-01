9a2b2c0ecb0ae37ea368bd64ae682ec589a0aa2e
harryle <lehoangsonsg@gmail.com>
2024-05-02 09:17:03 +0930

    ci: add ci files

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    coverage: {
+      thresholds: {
+        lines: 60,
+        branches: 60,
+        functions: 60,
+        statements: 60
+      },
+      enabled: true,
+      provider: "istanbul",
+      reporter: ['json-summary', 'json', 'text'],
+      reportOnFailure: true,
+      include: ["src/components/**"],
+      exclude: [
+        "**/*.stories.[jt]sx",
+        "**/*.helper.[jt]sx",
+        "**/*.helpers.[jt]sx",
+        "**/*.types.ts",
+        "src/components/accordion/**",
+        "src/components/dialog/**",
+        "src/components/navbar/**",
+        "src/components/button/**",
+      ],
+    },
     global: true,
     environment: "jsdom",
     setupFiles: "./setupTest",
     css: true,
   },
   plugins: [react()],
-})
+});
