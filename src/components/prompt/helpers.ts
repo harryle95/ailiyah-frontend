22796bcdc9d50c1919131a8bb0e81495fa74f741
harryle <lehoangsonsg@gmail.com>
2024-05-08 00:29:18 +0930

    feat: update submit method for put request and require request id

--- src/components/prompt/helpers.ts
+++ src/components/prompt/helpers.ts
@@ -5,26 +5,37 @@ const doSubmission = (
   text: Array<string>,
   id: Array<string | null>,
   images: Array<File>,
-  submit: SubmitFunction
+  submit: SubmitFunction,
+  method: "POST" | "post" | "PUT" | "put",
+  request_id?: string
 ) => {
   const submitFormData = new FormData();
   submitFormData.append("id", JSON.stringify(id));
   submitFormData.append("text", JSON.stringify(text));
   images.forEach((item) => submitFormData.append("images", item));
-  submit(submitFormData, { method: "POST", encType: "multipart/form-data" });
+  if (method.toLowerCase() === "put") {
+    if (request_id) {
+      submitFormData.append("request_id", request_id);
+    } else {
+      throw new Error("PUT method requires request_id to be provided");
+    }
+  }
+  submit(submitFormData, { method: method, encType: "multipart/form-data" });
 };
 
 const submitPrompt = (submit: SubmitFunction, prompt: string) => {
   const id = [null];
   const images = [new File([""], "empty.jpeg", { type: "image/jpeg" })];
   const text = [prompt];
-  doSubmission(text, id, images, submit);
+  doSubmission(text, id, images, submit, "POST");
 };
 
 const submitForm = (
   submit: SubmitFunction,
   formData: FormDataType,
-  intialFormData?: FormDataType
+  intialFormData?: FormDataType,
+  method: "POST" | "post" | "PUT" | "put" = "POST",
+  request_id?: string
 ) => {
   if (Object.entries(formData).length == 0) {
     throw new Error("Form must contain contents");
@@ -47,7 +58,7 @@ const submitForm = (
       id.push(null);
     }
   }
-  doSubmission(text, id, images, submit);
+  doSubmission(text, id, images, submit, method, request_id);
 };
 
 export { submitForm, submitPrompt };
