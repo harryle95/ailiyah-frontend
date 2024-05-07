95cbae68d005f428c2a31d3df9b6734fe7481199
harryle <lehoangsonsg@gmail.com>
2024-05-07 10:06:45 +0930

    feat: fix prompt submission error (e.preventDefault missing)

--- src/components/prompt/form.tsx
+++ src/components/prompt/form.tsx
@@ -3,23 +3,15 @@ import { FormDataType, StateType } from "@ailiyah-ui/prompt";
 import { styled } from "@ailiyah-ui/factory";
 import { Prompt } from "@ailiyah-ui/prompt";
 import { Form as _Form, useSubmit } from "react-router-dom";
+import { submitForm } from "./helpers";
 
-const Form = styled(_Form)
+const Form = styled(_Form);
 
 const PromptForm: React.FC<{
   initialFormData?: FormDataType;
   editing: boolean;
-  onSubmit?: React.FormEventHandler<HTMLFormElement>;
-  setOpen?:Function;
-}> = ({
-  setOpen,
-  initialFormData = undefined,
-  editing = true,
-  onSubmit = (e) => {
-    e.preventDefault();
-    alert("Submitting Form");
-  },
-}) => {
+  setOpen?: Function;
+}> = ({ setOpen, initialFormData = undefined, editing = true }) => {
   const submit = useSubmit();
   const initState = initialFormData
     ? React.useCallback(() => {
@@ -44,36 +36,15 @@ const PromptForm: React.FC<{
   });
 
   const onSubmitHandler = (e: React.FormEvent) => {
-    const text = new Array();
-    const images = new Array();
-    const id = new Array();
-
-    for (const pair of Object.entries(formData)){
-      let [key, {prompt, thumbnail}] = pair;
-      text.push(prompt);
-      if (thumbnail){
-        images.push(thumbnail)
-      }else{
-        images.push(new File([""], "empty.jpeg", {type: "image/jpeg"}))
-      }
-      if (!editing){
-        id.push(key)
-      }else{
-        id.push(null)
-      }
-    }
-    const submitFormData = new FormData();
-    submitFormData.append("id", JSON.stringify(id))
-    submitFormData.append("text", JSON.stringify(text))
-    images.forEach(item => submitFormData.append("images", item))
-    submit(submitFormData, {method: "POST", encType: "multipart/form-data", navigate: false} )
-    if (setOpen){
+    e.preventDefault();
+    if (setOpen) {
       setOpen(false);
     }
+    submitForm(submit, formData, initialFormData);
   };
 
   return (
-    <Form navigate={true} reloadDocument={true} onSubmit={onSubmitHandler} themeName="PromptForm">
+    <Form onSubmit={onSubmitHandler} themeName="PromptForm">
       <Prompt.Root
         editingStates={editingStates}
         setEditingStates={setEditingStates}
